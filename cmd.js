6f0a4e2e6c49e63ebaa2263304dcdc4346640cb4
庄文达 <pkwenda@163.com>
2019-12-11 12:56:19 +0800

    fix-bug-master   格式调整 http://39.104.96.233:60888/zentao/bug-view-master.html

--- cmd.js
+++ cmd.js
@@ -12,12 +12,12 @@ let table2 = new Table2({
 })
 
 table2.push(
-  ['查看帮助', 'help', '-h', 'zgit -h 或 zgit --help'],
-  ['查看 「 wekan 」列表 (敬请期待)', 'feature', 'f', 'zgit f 或 zgit feature'],
-  ['查看 「 禅 道 」列表 ', 'bug', 'b', 'zgit b 或 zgit bug'],
-  ['可以自动填写 commit 信息', 'commit', 'c', 'zgit c 或 zgit commit 后面可跟 msg:「zgit c 这是一次提交」 '],
-  ['自动 push', 'push', 'p', 'zgit p 或 zgit push'],
-  ['切换分支', 'checkout', 'co', 'zgit co 或 zgit checkout']
+  ['查看帮助',                      'help',      '-h',  'zgit -h 或 zgit --help'],
+  ['查看 「 wekan 」列表 (敬请期待)', 'feature',   'f',   'zgit f 或 zgit feature'],
+  ['查看 「 禅 道 」列表 ',          'bug',       'b',    'zgit b 或 zgit bug'],
+  ['可以自动填写 commit 信息',       'commit',    'c',    'zgit c 或 zgit commit 后面可跟 msg:「zgit c 这是一次提交」 '],
+  ['自动 push',                    'push',      'p',    'zgit p 或 zgit push'],
+  ['切换分支',                      'checkout',  'co',   'zgit co 或 zgit checkout']
 )
 
 ZingCMD.prototype.listening = function () {
