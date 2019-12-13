ff3748b1aac4c395d13779dad90100561511e7c0
庄文达 <pkwenda@163.com>
2019-12-13 20:00:43 +0800

    fix-bug-master   msg http://39.104.96.233:60888/zentao/bug-view-master.html

--- sql.js
+++ sql.js
@@ -4,22 +4,26 @@ var db = new sqlite3.Database("./zgit.db", function(e){
 //  if (err) throw err;
 });
 db.serialize(function() {
-//   db.run("  CREATE TABLE lorem (info TEXT)");
-//   db.run(`  CREATE TABLE USER(
-//             ID INT PRIMARY KEY                  NOT NULL,
-//             chandao_name                TEXT    NOT NULL,
-//             chandao_password            TEXT    NOT NULL,
-//             wekan_name                  TEXT    NOT NULL,
-//             wekan_password              TEXT    NOT NULL
-//  )`);
+ 
+ // 存贮用户信息的表   
+ db.run(`  CREATE TABLE IF NOT EXISTS USER(
+            ID INT PRIMARY KEY                  NOT NULL,
+            chandao_name                TEXT    NOT NULL,
+            chandao_password            TEXT    NOT NULL,
+            chandao_cookie              TEXT    NOT NULL,
+            wekan_name                  TEXT    NOT NULL,
+            wekan_password              TEXT    NOT NULL,
+            wekan_cookie                TEXT    NOT NULL
+ )`);
 
-//  // type 'close' 已关闭
-//  db.run(`  CREATE TABLE BUG(
-//     ID INT PRIMARY KEY                  NOT NULL,
-//     bug_id                      TEXT    NOT NULL,
-//     type                        TEXT    NOT NULL, 
-//     url                         TEXT    NOT NULL
-// )`);
+ // 存储 bug 的表
+ // type 'close' 已关闭
+ db.run(`  CREATE TABLE IF NOT EXISTS BUG(
+            ID INT PRIMARY KEY                  NOT NULL,
+            bug_id                      TEXT    NOT NULL,
+            type                        TEXT    NOT NULL, 
+            url                         TEXT    NOT NULL
+)`);
 
   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
   for (var i = 0; i < 10; i++) {
